3525563308a1865427ab56a3e1665a7c450c2707
Trần Tuấn Anh <anhtt@hvttgroup.com>
2024-01-04 22:33:00 +0700

    Push layout register
    Register

---
+++ src/routes/Login.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import '../routes/Login.css';
+import GoogleIcon from '../assets/images/icons8-google-48.png';
+
+const Login = () => {
+  return (
+    <div className="flex min-h-screen">
+      {/* Form đăng nhập bên trái */}
+      <div className="w-3/5 bg-white-800 p-8 text-black signup-left">
+        <h2 className="text-4xl mb-3 welcome">Welcome to monday.com</h2>
+        <h2 className="text-3x2 mb-4 started">Get started - it's free. No credit card needed.</h2>
+        <form>
+          {/* Các trường đăng nhập */}
+          <div className="mb-4 flex flex-col items-center">
+            <button
+              className="bg-white p-2 w-3/5 rounded-md flex items-center justify-center border border-gray-300 hover:border-gray-400 transition-all googleButton              "
+              type="button"
+            >
+              <img
+                src={GoogleIcon}
+                alt="Google Logo"
+                className="w-6 h-6 mr-2"
+              />
+              Continue with Google
+            </button>
+            {/* Dòng gạch ngang màu đen với chữ "Or" ở giữa */}
+            <div className="flex items-center mt-4 mb-4 w-3/5">
+              <hr className="flex-1 border-t border-gray" />
+              <span className="mx-4 text-gray-400">Or</span>
+              <hr className="flex-1 border-t border-gray" />
+            </div>
+            <input
+              className="p-2 border rounded-md w-3/5"
+              type="text"
+              id="username"
+              name="username"
+              placeholder="name@company.com"
+            />
+          </div>
+          {/* ... */}
+          <button className="w-3/5 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-all" type="submit">
+            Continue
+          </button>
+          {/* Dòng "Have account? Log in." */}
+          <div className="w-full text-center mt-4 text-gray-400">
+            Already have an account? <a href="#">Log in</a>.
+          </div>
+        </form>
+      </div>
+      {/* Ảnh nền bên phải */}
+      <div className="w-2/5 bg-cover bg-center signup-right"></div>
+    </div>
+  );
+};
+
+export default Login;
